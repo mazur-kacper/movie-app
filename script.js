c1f848fe4a3d503de596168ba3379bfd8e01bb99
mazur-kacper <109112557+mazur-kacper@users.noreply.github.com>
2026-04-15 23:33:39 +0200

    Add loading spinner

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let tytul = document.getElementById("tytul");
 let sprawdz = document.getElementById("sprawdz");
 let dane = document.getElementById("dane");
+let spinner = document.getElementById("spinner");
 
 sprawdz.addEventListener("click", function () {
   let nazwaFilmu = tytul.value;
@@ -9,10 +10,12 @@ sprawdz.addEventListener("click", function () {
     alert("Wpisz tytuł filmu!");
     return;
   }
+  spinner.style.display = "block";
 
   fetch(`http://www.omdbapi.com/?t=${nazwaFilmu}&apikey=891b8ba5`)
     .then((response) => response.json())
     .then((data) => {
+      spinner.style.display = "none";
       if (data.Response === "False") {
         dane.innerHTML = `<p>Nie znaleziono filmu!</p>`;
         return;
